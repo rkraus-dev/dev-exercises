36e84b6001c16bb7e56212aa8ac5d6a7b996981c
rkraus-dev <krobert98@outlook.de>
2026-06-30 10:53:02 +0200

    feat(control-flow): add element power switch expression exercise

--- csharp-basics/control-flow/switch/exercise-2-element-power/Program.cs
+++ csharp-basics/control-flow/switch/exercise-2-element-power/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string element = "Fire";
+
+string power = element switch
+{
+    "Fire" => "You wield Inferno Blast – enemies fear your flames.",
+    "Water" => "You wield Tidal Wave – the ocean bends to your will.",
+    "Wind" => "You wield Storm Gale – you move faster than the eye can see.",
+    "Earth" => "You wield Terra Crush – mountains crumble before you.",
+    _ => "Unknown element – have you even awakened yet?"
+};
+
+Console.WriteLine(power);
