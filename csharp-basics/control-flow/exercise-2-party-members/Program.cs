e2a51b6cfd9460ff6e844a45280615ee8c6d3ea4
rkraus-dev <krobert98@outlook.de>
2026-06-30 11:44:57 +0200

    feat(control-flow): add party members foreach exercise

--- csharp-basics/control-flow/exercise-2-party-members/Program.cs
+++ csharp-basics/control-flow/exercise-2-party-members/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string[] partyMembers = { "Subaru", "Rimuru", "Ainz", "Tanya" };
+
+foreach (string member in partyMembers)
+{
+    Console.WriteLine($"{member} has joined the party.");
+}
