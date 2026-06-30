268e2c42d1eb295c655966b8ec05f3300127be73
rkraus-dev <krobert98@outlook.de>
2026-06-30 11:25:09 +0200

    feat(control-flow): add quest reward nested ternary exercise

--- csharp-basics/control-flow/ternary/exercise-2-quest-reward/Program.cs
+++ csharp-basics/control-flow/ternary/exercise-2-quest-reward/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int questDifficulty = 7;
+
+string reward = questDifficulty == 10 ? "Reward: Mythic Artifact" : questDifficulty >= 8 ? "Reward: Legendary Sword" : "Reward: Common Potion";
+
+Console.WriteLine(reward);
