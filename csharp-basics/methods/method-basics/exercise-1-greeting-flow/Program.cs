0a4aa11731df7a1c048dde0f2ee4319915289a1e
rkraus-dev <krobert98@outlook.de>
2026-07-01 09:33:45 +0200

    feat(methods): add exercise-1-greeting-flow with basic method calls

--- csharp-basics/methods/method-basics/exercise-1-greeting-flow/Program.cs
+++ csharp-basics/methods/method-basics/exercise-1-greeting-flow/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿void PrintGreeting()
+{
+    Console.WriteLine("Hello, adventurer!");
+}
+
+void PrintDivider()
+{
+    Console.WriteLine("--------------------");
+}
+
+void PrintFarewell()
+{
+    Console.WriteLine("Safe travels!");
+}
+
+PrintGreeting();
+PrintDivider();
+PrintFarewell();
