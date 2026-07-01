fca1913938022ca02b1ae7ad603da36cfaeb7a64
rkraus-dev <krobert98@outlook.de>
2026-07-01 08:54:44 +0200

    feat(interview-prep): solve fizzbuzz

--- interview-prep/fizzbuzz/Program.cs
+++ interview-prep/fizzbuzz/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+
+}
