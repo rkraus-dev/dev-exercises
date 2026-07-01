c09b220e2542f1bbc1d46d2e6efdc6f6338cdc21
rkraus-dev <krobert98@outlook.de>
2026-07-01 12:57:24 +0200

    feat(interview-prep): solve calculator

--- interview-prep/calculator/Program.cs
+++ interview-prep/calculator/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public class Calculator
+{
+    int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    double Add(double a, double b)
+    {
+        return a +b;
+    }
+
+    double Add(double a, double b, int decimals)
+    {
+        return (a + b) ; // wie mach ich das mit deciamls?
+    }
+}
