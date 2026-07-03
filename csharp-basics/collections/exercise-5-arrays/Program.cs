d9b5fc902bb5dac2a22766ba94d3331f10205e6b
rkraus-dev <krobert98@outlook.de>
2026-07-03 15:09:56 +0200

    feat(collections): solve exercise-5-arrays

--- csharp-basics/collections/exercise-5-arrays/Program.cs
+++ csharp-basics/collections/exercise-5-arrays/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string[] weapons = { "Sword", "Shield", "Bow", "Axe", "Dagger" };
+Console.WriteLine("Inventory:");
+for (int i = 0; i < weapons.Length; i++)
+{
+    Console.WriteLine($"[{i}] {weapons[i]}");
+}
+
+Console.WriteLine();
+
+// Find the strongest weapon
+int length = 0;
+string strongestWeapon = "";
+foreach (string weapon in weapons)
+{
+    if (weapon.Length > length)
+    {
+        length = weapon.Length;
+        strongestWeapon = weapon;
+    }
+
+}
+
+Console.WriteLine($"Strongest Weapon: {strongestWeapon} ({length} chars)");
+Console.WriteLine();
+
+// Replace one weapon 
+Console.WriteLine("Replacing slot 2...");
+Console.WriteLine();
+weapons[2] = "Crossbow";
+Console.WriteLine("Updated Inventory: ");
+for (int i = 0; i < weapons.Length; i++)
+{
+    Console.WriteLine($"[{i}] {weapons[i]}");
+}
