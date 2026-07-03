1570c03b79cc6b506213358ef5c29a2c8e83c596
rkraus-dev <krobert98@outlook.de>
2026-07-03 15:46:41 +0200

    feat(collections): solve exercise-6-2d-arrays

--- csharp-basics/collections/exercise-6-2d-arrays/Program.cs
+++ csharp-basics/collections/exercise-6-2d-arrays/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int enemyCount = 0;
+string[,] grid =
+{
+     {".", "E", "."},
+     {"A", ".", "E"},
+     {".", ".", "A"}
+};
+
+for (int i = 0; i < grid.GetLength(0); i++)
+{
+    for (int j = 0; j < grid.GetLength(1); j++)
+    {
+        Console.Write(grid[i, j] + " ");
+        if (grid[i, j] == "E")
+        {
+            enemyCount++;
+        }
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine($"Enemy Count: {enemyCount}");
+
+// Find first enemy
+for (int i = 0; i < grid.GetLength(0); i++)
+{
+    for (int j = 0; j < grid.GetLength(1); j++)
+    {
+        if (grid[i, j] == "E")
+        {
+            Console.WriteLine($"First enemy at: [{i}, {j}]");
+            return;
+        }
+    }
+}
+
+
