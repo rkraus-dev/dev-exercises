e1b070535cd0d57610ebc32a2ca918de855e384d
rkraus-dev <krobert98@outlook.de>
2026-07-07 13:48:31 +0200

    feat(collections): solve exercise-11-loot-system

--- csharp-basics/collections/exercise-11-loot-system/Program.cs
+++ csharp-basics/collections/exercise-11-loot-system/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string[] drops = { "Coin", "Slime", "Feather" };
+
+Dictionary<string, int> dropCounts = new Dictionary<string, int>();
+
+// fill drops
+foreach (string drop in drops)
+{
+    dropCounts.Add(drop, 0);
+}
+
+// manuell setting count of items
+dropCounts["Coin"] = 200;
+dropCounts["Slime"] = 87;
+dropCounts["Feather"] = 10;
+
+HashSet<string> rareItems = new HashSet<string>() { "Sword" };
+
+// Print all
+Console.WriteLine($"Possible drops: ");
+foreach (string drop in drops)
+{
+    Console.WriteLine(drop);
+}
+Console.WriteLine();
+Console.WriteLine("Collected Drops: ");
+foreach (KeyValuePair<string, int> drop in dropCounts)
+{
+    Console.WriteLine($"{drop.Key}: {drop.Value}");
+}
+
+Console.WriteLine();
+Console.WriteLine($"Try adding sword to list.... Success: {rareItems.Add("Sword")}");
+Console.WriteLine("Legendary drop:");
+foreach (string legendary in rareItems)
+{
+    Console.WriteLine(legendary);
+}
