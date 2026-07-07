ba783a21251417efc082c8221a7049e95f36d64f
rkraus-dev <krobert98@outlook.de>
2026-07-07 14:43:28 +0200

    feat(interview-prep): solve array-reverse

--- interview-prep/array-reverse/Program.cs
+++ interview-prep/array-reverse/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int[] numbers = { 1, 2, 3, 4, 5 };
+
+// output original
+Console.Write("Original: ");
+foreach (int number in numbers)
+{
+    Console.Write(number + " ");
+}
+
+Console.WriteLine();
+
+// output reverse
+int indexer = 1;
+for(int i = 0; i < numbers.Length / 2; i++)
+{
+    int temp = numbers[i];
+    numbers[i] = numbers[numbers.Length - indexer];
+    numbers[numbers.Length - indexer] = temp;
+    indexer++;
+
+}
+
+Console.Write("Reversed: ");
+foreach (int number in numbers)
+{
+    Console.Write(number + " ");
+}
