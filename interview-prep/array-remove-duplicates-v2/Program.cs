0711cf4134ae69dbde185f419977d18b64ec9042
rkraus-dev <krobert98@outlook.de>
2026-07-07 15:48:30 +0200

    feat(interview-prep): solve array-remove-duplicates-v2

--- interview-prep/array-remove-duplicates-v2/Program.cs
+++ interview-prep/array-remove-duplicates-v2/Program.cs
@@ -1,1 +1,38 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int[] numbers = { 1, 2, 3, 2, 4, 3, 5, 1 };
+int[] cleaned = new int[numbers.Length];
+
+int counter = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    bool isDuplicate = false;
+
+    for (int j = 0; j < counter; j++)
+    {
+        if (cleaned[j] == numbers[i])
+        {
+            isDuplicate = true;
+        }
+    }
+
+    if (!isDuplicate)
+    {
+        cleaned[counter] = numbers[i];
+        counter++;
+    }
+}
+
+// Print
+Console.Write("Original: ");
+foreach (int number in numbers)
+{
+    Console.Write(number + " ");
+}
+
+Console.WriteLine();
+Console.Write("Cleaned: ");
+foreach (int clean in cleaned)
+{
+    if (clean != 0)
+        Console.Write(clean + " ");
+}
