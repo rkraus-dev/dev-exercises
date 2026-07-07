62522f1939d34ae9f97f10d614953f78eaa1c46e
rkraus-dev <krobert98@outlook.de>
2026-07-07 09:22:43 +0200

    feat(collections): solve exercise-7-list

--- csharp-basics/collections/exercise-7-list/Program.cs
+++ csharp-basics/collections/exercise-7-list/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿List<string> heroes = new List<string>(){"Archer", "Mage", "Warrior"};
+
+// Print list
+Console.WriteLine("Party:");
+for(int i = 0; i < heroes.Count; i++)
+{
+    Console.WriteLine($"[{i}] {heroes[i]}");
+}
+Console.WriteLine();
+
+// Add a hero
+Console.WriteLine("Adding Healer...");
+heroes.Add("Healer");
+
+// Removing a hero
+Console.WriteLine("Removing Mage...");
+heroes.Remove("Mage");
+
+// Is Healer in party?
+Console.WriteLine();
+Console.WriteLine($"Healer in party: {heroes.Contains("Healer")}");
+
+// Sort and print list
+heroes.Sort();
+Console.WriteLine();
+Console.WriteLine("Sorted Party:");
+for(int i = 0; i < heroes.Count; i++)
+{
+    Console.WriteLine($"[{i}] {heroes[i]}");
+}
