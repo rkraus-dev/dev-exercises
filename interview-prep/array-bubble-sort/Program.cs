385d384cb57f559ef7712bbda4007623b22798e3
rkraus-dev <krobert98@outlook.de>
2026-07-08 09:26:52 +0200

    feat(interview-prep): solve array-bubble-sort

--- interview-prep/array-bubble-sort/Program.cs
+++ interview-prep/array-bubble-sort/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int[] attackStats = { 42, 17, 85, 3, 66, 29 };
+bool hasSwitched = false; // tracks if numbers have been switched
+int num = 0;
+
+// print original
+Console.Write("Original: ");
+foreach(int attack in attackStats)
+{
+    Console.Write(attack + " ");
+}
+
+// Sort array
+while (!hasSwitched)
+{
+    hasSwitched = true; // assume no switches will be made
+    for (int i = 0; i < attackStats.Length - 1; i++)
+    {
+        if (attackStats[i] > attackStats[i + 1])
+        {
+            num = attackStats[i];
+            attackStats[i] = attackStats[i + 1];
+            attackStats[i + 1] = num;
+            hasSwitched = false;
+        }
+    }
+}
+
+// Print sorted array
+Console.WriteLine();
+Console.Write("Sorted: ");
+foreach(int attack in attackStats)
+{
+    Console.Write(attack + " ");
+}
+
