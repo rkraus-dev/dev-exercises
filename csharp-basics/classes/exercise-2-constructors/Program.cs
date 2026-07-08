3129c166a8e26b741f9e8f05409221c1fcec9d4b
rkraus-dev <krobert98@outlook.de>
2026-07-08 11:33:39 +0200

    feat(classes): add exercise-2-constructors with overloaded constructors and constructor chaining

--- csharp-basics/classes/exercise-2-constructors/Program.cs
+++ csharp-basics/classes/exercise-2-constructors/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Item itm1 = new Item("Sword", 54, 10);
+itm1.PrintInfo();
+
+Item itm2 = new("Wand");
+itm2.PrintInfo();
+
+
+class Item
+{
+    private string name;
+    private int damage;
+    private int resistance;
+
+    public Item(string name, int damage, int resistance)
+    {
+        this.name = name;
+        this.damage = damage;
+        this.resistance = resistance;
+    }
+
+    public Item(string name) : this(name, 10, 5)
+    {
+        
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Name: {name}, Damage: {damage}, Resistance: {resistance}");
+    }
+}
