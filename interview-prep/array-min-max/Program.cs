c7caa8257d448f8d27bda53ffafcaed1d9aadddd
rkraus-dev <krobert98@outlook.de>
2026-07-08 08:37:00 +0200

    feat(interview-prep): solve array-min-max

--- interview-prep/array-min-max/Program.cs
+++ interview-prep/array-min-max/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int[] stats = { 42, 17, 85, 3, 66, 29 };
+int biggestNum = stats[0];
+int lowestNum = stats[0];
+
+Console.Write("Stats: ");
+foreach (int stat in stats)
+{
+    Console.Write(stat + " ");
+
+    if(biggestNum < stat)
+    {
+        biggestNum = stat;
+    }
+
+    if(lowestNum > stat)
+    {
+        lowestNum = stat;
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine($"Max: {biggestNum}");
+Console.WriteLine($"Min: {lowestNum}");
