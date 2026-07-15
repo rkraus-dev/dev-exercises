72e3965f7ceb4a37742f2ea2abf4b05ff0ffbf71
rkraus-dev <krobert98@outlook.de>
2026-07-15 16:28:32 +0200

    chore(interview-prep): scaffold palindrome-check

---
+++ interview-prep/palindrome-check/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Enter a word: ");
+string input = Console.ReadLine() ?? "";
+string userInput = input.Trim().ToLower();
+int length = userInput.Length;
+int middle = userInput.Length / 2;
+bool isPalindrom = true;
+
+for (int i = 0; i < middle; i++)
+{
+    length--;
+    if (userInput[i] != userInput[length])
+    {
+        isPalindrom = false;
+        break;
+    }
+
+}
+
+if (isPalindrom)
+    Console.WriteLine($"{userInput} is a palindrome!");
+else
+    Console.WriteLine($"{userInput} is not a palindrome!");
