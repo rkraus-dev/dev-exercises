16d43e652151e15fd58bc74567fca24850f32155
rkraus-dev <krobert98@outlook.de>
2026-07-15 15:46:34 +0200

    feat(control-flow): solve exercise-5-number-guessing

--- csharp-basics/control-flow/loops/exercise-5-number-guessing/Program.cs
+++ csharp-basics/control-flow/loops/exercise-5-number-guessing/Program.cs
@@ -1,1 +1,44 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int goal = 7;
+int counter = 0;
+bool hasGuessed = false;
+
+do
+{
+    Console.Write("Guess a number between 1 and 10: ");
+
+    //user input
+    string input = Console.ReadLine() ?? "";
+    // check if input is null
+    if (!int.TryParse(input, out int userGuess))
+    {
+        Console.WriteLine("Please enter a valid number!");
+        Console.WriteLine();
+    }
+    else if (userGuess == goal)
+    {
+        Console.WriteLine($"Correct! You guessed it in {counter} attempts.");
+    }
+    else if (userGuess >= 1 && userGuess <= 10)
+    {
+        if (userGuess > goal)
+        {
+            Console.WriteLine("Too high!");
+        }
+        else if (userGuess < goal)
+        {
+            Console.WriteLine("Too low!");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Please enter a number between 1 and 10!");
+        Console.WriteLine();
+    }
+
+    counter++;
+
+} while (!hasGuessed);
+
+
+
+
