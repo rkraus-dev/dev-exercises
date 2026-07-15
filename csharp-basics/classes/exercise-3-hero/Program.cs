bf445615526a86922b7f836d50303450af7d26d8
rkraus-dev <krobert98@outlook.de>
2026-07-15 10:16:02 +0200

    feat(classes): solve exercise-3-hero

--- csharp-basics/classes/exercise-3-hero/Program.cs
+++ csharp-basics/classes/exercise-3-hero/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Hero hero1 = new Hero("Arthur", 25);
+Hero hero2 = new Hero("Merlin", 47);
+Hero hero3 = new Hero("Lancelot", 30);
+
+hero1.Introduce();
+hero2.Introduce();
+hero3.Introduce();
+Hero.PrintHeroCount();
+
+
+class Hero
+{
+    private string _name;
+    private int _age;
+    private static int _heroCounter = 0;
+
+    public Hero(string name, int age)
+    {
+        this._name = name;
+        this._age = age;
+        _heroCounter++;
+    }
+
+    public void Introduce()
+    {
+        Console.WriteLine($"Hi, my name is {_name} and I am {_age} years old.");
+    }
+
+    public static void PrintHeroCount()
+    {
+        Console.WriteLine($"Total Heroes: {_heroCounter}");
+    }
+
+}
