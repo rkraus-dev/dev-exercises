9c838b8846d88122079b18311b3dcdbff2f432f6
rkraus-dev <krobert98@outlook.de>
2026-07-15 15:57:16 +0200

    feat(methods): solve exercise-2-swap

--- csharp-basics/methods/exercise-2-swap/Program.cs
+++ csharp-basics/methods/exercise-2-swap/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int a = 10;
+int b = 25;
+
+void Swap(ref int a, ref int b)
+{
+    int save = b;
+    b = a;
+    a = save;
+
+}
+
+Console.WriteLine($"Before: a = {a}, b = {b}");
+
+Swap(ref a, ref b);
+
+Console.WriteLine($"After: a = {a}, b = {b}");
