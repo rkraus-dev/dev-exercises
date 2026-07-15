1517e16d738d80dd61f52a51155c54ecca807e66
rkraus-dev <krobert98@outlook.de>
2026-07-15 10:59:44 +0200

    feat(classes): add exercise-4-bank-account demonstrating properties with validation

--- csharp-basics/classes/exercise-4-properties/Program.cs
+++ csharp-basics/classes/exercise-4-properties/Program.cs
@@ -1,1 +1,41 @@
-﻿Console.WriteLine("Hello, World!");
+﻿BankAccount account1 = new BankAccount("Rob", 1234);
+account1.Balance = 999.99;
+Console.WriteLine(account1.Balance);
+account1.Balance = -99;
+Console.WriteLine(account1.Balance);
+Console.WriteLine(account1.AccountId);
+
+class BankAccount
+{
+    private double balance;
+    public string Owner { get; set; }
+
+    public double Balance
+    {
+        get
+        {
+            return balance;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                balance = 0;
+            }
+            else
+            {
+                balance = value;
+            }
+
+        }
+    }
+
+    public int AccountId { get; private set; }
+
+    public BankAccount(string owner, int accountId)
+    {
+        Owner = owner;
+        AccountId = accountId;
+    }
+
+}
