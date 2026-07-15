ae14ef38a12b71e3531d5ae2cc01b8dee05e9706
rkraus-dev <krobert98@outlook.de>
2026-07-15 14:50:20 +0200

    feat(classes): add exercise-6-abstract-class with Shape abstract class and Circle/Rectangle implementations

--- csharp-basics/classes/exercise-6-abstract-class/Program.cs
+++ csharp-basics/classes/exercise-6-abstract-class/Program.cs
@@ -1,1 +1,56 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Circle c1 = new Circle("Circle", 12.2);
+c1.PrintInfo();
+
+Rectangle r1 = new Rectangle("Rectangle", 3, 4);
+r1.PrintInfo();
+
+abstract class Shape
+{
+    public string Name { get; set; }
+
+    public Shape(string name)
+    {
+        Name = name;
+    }
+
+    public abstract double CalculateArea();
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Name: {Name}, Area: {CalculateArea()}");
+    }
+
+}
+
+class Circle : Shape
+{
+    public double Radius { get; set; }
+    public const double pi = 3.14;
+
+    public Circle(string name, double radius) : base(name)
+    {
+        Radius = radius;
+    }
+    public override double CalculateArea()
+    {
+        return pi * (Radius * Radius);
+    }
+}
+
+class Rectangle : Shape
+{
+    public double Height { get; set; }
+    public double Width { get; set; }
+
+    public Rectangle(string name, double height, double width) : base(name)
+    {
+        Height = height;
+        Width = width;
+    }
+
+    public override double CalculateArea()
+    {
+        return Height * Width;
+    }
+
+}
