4688a9fbfe2b4d64d691108ca33845acc855d4aa
rkraus-dev <krobert98@outlook.de>
2026-07-16 10:24:49 +0200

    feat(classes): add exercise-9-interface with IWorkable and IManageable demonstrating multiple interface implementation

--- csharp-basics/classes/exercise-9-interface/Program.cs
+++ csharp-basics/classes/exercise-9-interface/Program.cs
@@ -1,1 +1,43 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Manager manager = new Manager();
+Employee employee = new Employee();
+
+DoWork(manager);
+DoWork(employee);
+
+manager.ManageTeam();
+
+void DoWork(IWorkable worker)  
+{
+    worker.Work();
+}
+
+interface IWorkable
+{
+    void Work();
+}
+
+interface IManageable
+{
+    void ManageTeam();
+}
+
+class Manager : IWorkable, IManageable
+{
+    public void Work()
+    {
+        Console.WriteLine("Manager is working.");
+    }
+
+    public void ManageTeam()
+    {
+        Console.WriteLine("Manager is managing the team.");
+    }
+}
+
+class Employee : IWorkable
+{
+    public void Work()
+    {
+        Console.WriteLine("Employee is working.");
+    }
+}
