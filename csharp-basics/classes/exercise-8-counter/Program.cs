697e85c8e3e7343af77fa2e1d18da0d3a89c9752
rkraus-dev <krobert98@outlook.de>
2026-07-16 09:43:50 +0200

    feat(classes): solve exercise-8-counter

--- csharp-basics/classes/exercise-8-counter/Program.cs
+++ csharp-basics/classes/exercise-8-counter/Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Hero hero1 = new Hero("Arthur");
+Console.WriteLine($"{hero1.Name} joined the guild.");
+Hero.PrintCount();
+
+Hero hero2 = new Hero("Merlin");
+Console.WriteLine($"{hero2.Name} joined the guild.");
+Hero.PrintCount();
+
+Hero hero3 = new Hero("Lancelot");
+Console.WriteLine($"{hero3.Name} joined the guild.");
+Hero.PrintCount();
+
+class Hero
+{
+    private string _name;
+    private static int _heroCount = 0;
+
+    public string Name
+    {
+        get { return _name;}
+    }
+
+    public static int HeroCount
+    {
+        get{ return _heroCount;}
+    }
+
+    public Hero(string name)
+    {
+        _name = name;
+        _heroCount++;
+        
+    }
+
+    public static void PrintCount()
+{
+    Console.WriteLine($"Total heroes: {_heroCount}");
+}
+
+}
