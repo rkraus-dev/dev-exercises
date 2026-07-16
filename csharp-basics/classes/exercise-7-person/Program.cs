c671b3c0a95776e5a0e1597fdd749ebe3c0e2b54
rkraus-dev <krobert98@outlook.de>
2026-07-16 09:21:28 +0200

    feat(classes): solve exercise-7-person

--- csharp-basics/classes/exercise-7-person/Program.cs
+++ csharp-basics/classes/exercise-7-person/Program.cs
@@ -1,1 +1,49 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Person person1 = new Person("Arthur", 25);
+person1.Introduce();
+person1.Age = 26;
+Console.WriteLine($"{person1.Name} is now {person1.Age} years old.");
+person1.Age = -3;
+
+class Person
+{
+    private string _name;
+    private int _age;
+    public string Name
+{
+    get { return _name; }
+    set { _name = value; }
+}
+    public int Age
+    {
+        get
+        {
+            return _age;
+        }
+
+        set
+        {
+            if (value < 0)
+            {
+                _age = 0;
+                Console.WriteLine("Invalid Age!");
+            }
+            else
+            {
+                _age = value;
+            }
+        }
+
+    }
+
+    public Person(string name, int age)
+    {
+        Age = age;
+        Name = name;
+    }
+
+    public void Introduce()
+    {
+        Console.WriteLine($"Hi, my name is {Name} and I am {Age} years old.");
+    }
+
+}
