0d5c863aeaf9780da6eb13afab03acadf0fc2e6f
rkraus-dev <krobert98@outlook.de>
2026-07-18 12:38:31 +0200

    feat(collections): solve exercise-14-second-largest

--- csharp-basics/collections/exercise-14-second-largest/Program.cs
+++ csharp-basics/collections/exercise-14-second-largest/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int[] stats = { 42, 17, 85, 3, 66, 29 };
+
+// print stats
+Console.Write("Stats:");
+foreach(int stat in stats)
+{
+    Console.Write(stat + " ");
+}
+Console.WriteLine();
+
+//find largest number
+int largestNumber = stats[0];
+foreach(int stat in stats)
+{
+    if(stat > largestNumber)
+        largestNumber = stat;
+}
+Console.WriteLine($"Largest: {largestNumber}");
+
+// find second largest number
+int secondLargestNumber = stats[0];
+foreach(int stat in stats)
+{
+    if(stat == largestNumber)
+        continue;
+    else if(stat > secondLargestNumber)
+        secondLargestNumber = stat;
+    
+}
+Console.WriteLine($"Second largest: {secondLargestNumber}");
