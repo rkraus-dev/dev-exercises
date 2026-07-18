91ccb08a2f4fc07b304b126312bc2991f086d33a
rkraus-dev <krobert98@outlook.de>
2026-07-18 10:39:54 +0200

    feat(strings): add exercise-2-stringbuilder demonstrating StringBuilder operations

--- csharp-basics/strings/exercise-2-stringbuilder/Program.cs
+++ csharp-basics/strings/exercise-2-stringbuilder/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+List<string> shoppingList = new List<string> { "Steak", "Bananas", "Pasta", "Rice", "Chicken" };
+
+StringBuilder sb = new StringBuilder();
+
+int counter = 1;
+foreach (string item in shoppingList)
+{
+    sb.AppendLine(counter + ". " + item);
+    counter++;
+}
+
+sb.Insert(0, "Shopping List:\n");
+sb.Replace("Chicken", "Shrimps");
+
+Console.WriteLine(sb.ToString());
+Console.WriteLine($"Length: {sb.Length}");
