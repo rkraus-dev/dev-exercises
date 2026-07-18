f9041289e0f9d12b42132179657ce7f6ad236f57
rkraus-dev <krobert98@outlook.de>
2026-07-18 11:44:35 +0200

    feat(collections): solve exercise-12-most-common

--- csharp-basics/collections/exercise-12-most-common/Program.cs
+++ csharp-basics/collections/exercise-12-most-common/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿List<string> drops = new List<string> { "Sword", "Coin", "Coin", "Shield", "Coin", "Sword", "Potion", "Coin" };
+Dictionary<string, int> dropCount = new Dictionary<string, int>();
+
+Console.Write($"Drops:");
+foreach (string drop in drops)
+{
+    Console.Write(drop + " ");
+    if (dropCount.ContainsKey(drop))
+    {
+        dropCount[drop]++;
+    }
+    else
+    {
+        dropCount.Add(drop, 1);
+    }
+}
+
+string mostCommon = "";
+int highestCount = 0;
+foreach (KeyValuePair<string, int> value in dropCount)
+{
+    if (value.Value > highestCount)
+    {
+        mostCommon = value.Key;
+        highestCount = value.Value;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Most common drop: {mostCommon} ({highestCount}x)");
