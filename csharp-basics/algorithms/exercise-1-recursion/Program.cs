9bc5c1ecfca4ea42e5be2052b23e0f99dc14d424
rkraus-dev <krobert98@outlook.de>
2026-07-20 10:27:34 +0200

    feat(algorithms): add exercise-1-recursion with Factorial and Fibonacci implementations

--- csharp-basics/algorithms/exercise-1-recursion/Program.cs
+++ csharp-basics/algorithms/exercise-1-recursion/Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine($"Factorial: {Factorial(0)}");
+Console.WriteLine($"Factorial: {Factorial(1)}");
+Console.WriteLine($"Factorial: {Factorial(5)}");
+Console.WriteLine();
+
+Console.Write("Fibonacci: ");
+for(int i = 0; i <= 10; i++)
+{
+    Console.Write($"{Fibonacci(i)} ");
+}
+
+int Factorial(int n)
+{
+    if (n <= 1)
+    {
+        return 1;
+    }
+    return n * Factorial(n - 1);
+
+}
+
+
+int Fibonacci(int n)
+{
+    if(n <= 1)
+    {
+        return n;
+    }
+    return Fibonacci(n -1) + Fibonacci(n - 2);
+
+}
