1392272ed80ed3f6c935edf8309a122ee6b96d75
rkraus-dev <krobert98@outlook.de>
2026-07-20 15:06:05 +0200

    feat(collections): solve exercise-16-item-shop

--- csharp-basics/collections/exercise-16-item-shop/Program.cs
+++ csharp-basics/collections/exercise-16-item-shop/Program.cs
@@ -1,1 +1,45 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Dictionary<string, int> inventory = new Dictionary<string, int>
+{
+  {"Sword", 150},
+  {"Shield", 120},
+  {"Potion", 30},
+  {"Bow", 90},
+  {"Armor", 200}
+
+};
+Console.WriteLine("Shop inventory:");
+foreach (KeyValuePair<string, int> item in inventory)
+{
+    Console.WriteLine($"{item.Key}: {item.Value}");
+}
+
+Console.WriteLine();
+Console.WriteLine("Adding Elixir...");
+inventory.Add("Elixir", 80);
+Console.WriteLine("Price increase on Sword...");
+inventory["Sword"] = 175;
+Console.WriteLine();
+
+//check if dagger is in shop
+Console.WriteLine($"Dagger in shop: {inventory.ContainsKey("Dagger")}");
+
+//cheapest item
+int cheapestItemValue = int.MaxValue;
+string cheapestItemName = "";
+foreach (KeyValuePair<string, int> item in inventory)
+{
+    if (item.Value < cheapestItemValue)
+    {
+        cheapestItemValue = item.Value;
+        cheapestItemName = item.Key;
+    }
+}
+
+Console.WriteLine($"Cheapest Item: {cheapestItemName} ({cheapestItemValue})");
+
+Console.WriteLine();
+Console.WriteLine("Updated inventory:");
+foreach (KeyValuePair<string, int> item in inventory)
+{
+    Console.WriteLine($"{item.Key}: {item.Value}");
+}
