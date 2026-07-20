ea9d511d870890fc263c78b9bad046ad68f3cbe6
rkraus-dev <krobert98@outlook.de>
2026-07-20 12:56:53 +0200

    feat(leetcode): solve merge-strings-alternately

--- leetcode/merge-strings-alternately/Program.cs
+++ leetcode/merge-strings-alternately/Program.cs
@@ -1,1 +1,42 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var solution = new Solution();
+
+Console.WriteLine(solution.MergeAlternately("abcfg", "pqrs"));  
+Console.WriteLine(solution.MergeAlternately("ab", "pqrs"));   
+Console.WriteLine(solution.MergeAlternately("abcd", "pq"));  
+
+public class Solution
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+        string merged = "";
+
+        int shorterLength;
+        if (word1.Length < word2.Length)
+        {
+            shorterLength = word1.Length;
+        }
+        else
+        {
+            shorterLength = word2.Length;
+        }
+
+        for (int i = 0; i < shorterLength; i++)
+        {
+            merged += word1[i];
+            merged += word2[i];
+        }
+
+        for (int i = shorterLength; i < word1.Length; i++)
+        {
+            merged += word1[i];
+        }
+
+        for (int i = shorterLength; i < word2.Length; i++)
+        {
+            merged += word2[i];
+        }
+
+
+        return merged;
+    }
+}
