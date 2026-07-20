d9a7ecbdcc1555a4eddda800fb20b5d44434ab97
rkraus-dev <krobert98@outlook.de>
2026-07-20 11:03:13 +0200

    feat(algorithms): add exercise-2-searching with LinearSearch and BinarySearch implementations

--- csharp-basics/algorithms/exercise-2-searching/Program.cs
+++ csharp-basics/algorithms/exercise-2-searching/Program.cs
@@ -1,1 +1,47 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int[] unsortedArr = { 3, 14, 57, 1, 17, 20, 5, 8 };
+int[] sortedArr = { 1, 3, 5, 8, 14, 17, 20, 57 };
+
+Console.WriteLine($"Linear search for: 20 | Found at Index: {LinearSearch(unsortedArr, 20)}");
+Console.WriteLine($"Linear search for: 109 | Found at Index: {LinearSearch(unsortedArr, 109)}");
+Console.WriteLine();
+Console.WriteLine($"Binary search for: 57 | Found at Index: {BinarySearch(sortedArr, 57)}");
+Console.WriteLine($"Binary search for: 109 | Found at Index: {BinarySearch(sortedArr, 109)}");
+
+
+int LinearSearch(int[] numbers, int target)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] == target)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int BinarySearch(int[] numbers, int target)
+{
+    int left = 0;
+    int right = numbers.Length - 1;
+
+    while (left <= right)
+    {
+        int middle = (left + right) / 2;
+        if (numbers[middle] == target)
+        {
+            return middle;
+        }
+        else if (numbers[middle] < target)
+        {
+            left = middle + 1;
+        }
+        else
+        {
+            right = middle - 1;
+        }
+    }
+
+    return - 1;
+}
