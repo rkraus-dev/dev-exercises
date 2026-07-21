1ee28c634761623ea35377df1c154cf4e1b5afeb
rkraus-dev <krobert98@outlook.de>
2026-07-21 11:33:05 +0200

    chore(leetcode): scaffold guess-number-higher-or-lower

---
+++ leetcode/guess-number-higher-or-lower/Program.cs
@@ -0,0 +1,25 @@
+﻿var solution = new Solution();
+Console.WriteLine(solution.GuessNumber(10));
+Console.WriteLine(solution.GuessNumber(1));
+Console.WriteLine(solution.GuessNumber(2126753390));
+
+public class Solution : GuessGame
+{
+    public int GuessNumber(int n)
+    {
+        // TODO – dein Ratespiel
+        return 0;
+    }
+}
+
+public class GuessGame
+{
+    private int pick = 6;
+
+    protected int guess(int num)
+    {
+        if (num > pick) return -1;
+        if (num < pick) return 1;
+        return 0;
+    }
+}
